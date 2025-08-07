a4489b8e4104b06772fb754c17a9fc277deea507
Mashudu Molema <mrkaymp@gmail.com>
2025-08-07 14:26:31 +0200

    Defined base components and landing page.

--- alx-movie-app/components/layout/Button.tsx
+++ alx-movie-app/components/layout/Button.tsx
@@ -1,13 +1,11 @@
-import { GeneratedImageProps } from "@/interfaces";
+import { ButtonProps } from "@/interfaces";
 
-const ImageCard: React.FC<GeneratedImageProps> = ({ imageUrl, prompt, width, action }) => {
+const Button: React.FC<ButtonProps> = ({ title, action }) => {
   return (
-    <div onClick={() => action(imageUrl)} className="mt-6 border hover:cursor-pointer">
-      <img src={imageUrl} alt={prompt} className={`w-full max-w-md rounded-lg shadow-lg`} />
-      <h2 className={`${width ? 'text-sm' : 'text-xl'} font-semibold mt-2`}>Your Prompt:</h2>
-      <p className={`${width ? 'text-xs' : 'text-lg'} text-gray-700 mb-4`}>{prompt}</p>
-    </div>
+    <button onClick={action} className="px-8 py-2 border-2 border-[#E2D609] rounded-full hover:bg-[#E2D609] hover:text-black transition-colors duration-300">
+      {title}
+    </button>
   )
 }
 
-export default ImageCard;
+export default Button;
